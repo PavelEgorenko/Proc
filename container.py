7b43b254e8763792daafcd30c78a7b6008eac1e0
vipeg <vip.egorenko@mail.ru>
2021-04-15 12:53:01 +0700

    Multimethod

--- container.py
+++ container.py
@@ -1,9 +1,12 @@
 from matrices import InMtx
 from matrices import OutMtx
-from diagonal_matrix import OutDiag
-from binary_matix import OutBin
+from diagonal_matrix import OutDiag, diagonal_matrix
+from binary_matix import OutBin, bin_matrix
+
+
 class container:
     matrices = []
+
 
 def InData(matrices, file):
     for line in file:
@@ -16,11 +19,33 @@ def OutData(matrices, ofst):
         ofst.write(str(i + 1) + ": ")
         OutMtx(matrices[i], ofst)
 
+
 def Sort(matrices):
-    for i in range(len(matrices)-1):
-        for k in range(len(matrices)-1):
-            if matrices[k].size > matrices[k+1].size:
-                matrices[k], matrices[k+1] = matrices[k+1], matrices[k]
+    for i in range(len(matrices) - 1):
+        for k in range(len(matrices) - 1):
+            if matrices[k].size > matrices[k + 1].size:
+                matrices[k], matrices[k + 1] = matrices[k + 1], matrices[k]
+
 
 def Clear(matrices):
     matrices.clear()
+
+
+def Multimethods(matrices, ofst):
+    ofst.write("Multimethod\n")
+    for i in range(len(matrices) - 1):
+        for j in range(i, len(matrices)):
+            if isinstance(matrices[i], diagonal_matrix):
+                ofst.write("Diagonal ")
+                if isinstance(matrices[j], diagonal_matrix):
+                    ofst.write(" and diagonal matrix\n")
+                if isinstance(matrices[j], bin_matrix):
+                    ofst.write(" and common matrix\n")
+            if isinstance(matrices[i], bin_matrix):
+                ofst.write("Common ")
+                if isinstance(matrices[j], diagonal_matrix):
+                    ofst.write(" and diagonal matrix\n")
+                if isinstance(matrices[j], bin_matrix):
+                    ofst.write(" and common matrix\n")
+            OutMtx(matrices[i], ofst)
+            OutMtx(matrices[j], ofst)
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from container import *
 
 ifst = open("input.txt").read().split("\n")
 ofst = open("output.txt", "w")
+ofst2 = open("output2.txt", "w")
+
 
 print("Start.")
 c = container()
 InData(c.matrices, ifst)
 ofst.write("Filled container.\n")
+Multimethods(c.matrices, ofst2)
 Sort(c.matrices)
 OutData(c.matrices, ofst)
 Clear(c.matrices)
